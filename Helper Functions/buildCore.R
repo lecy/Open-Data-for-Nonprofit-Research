f569205aa7ab9cd99170ea7f042fcc3ee47f8dbe
Jesse Lecy <jdlecy@gmail.com>
2016-07-12 10:18:27 -0400

    added helper function

---
+++ Helper Functions/buildCore.R
@@ -0,0 +1,34 @@
+# FUNCTION TO BUILD THE CORE DATASET
+# 
+# Arguments: 
+#    ein - character vector of nonprofits to sample
+#    years - vector of which years to collect
+#    form.type - which type of data to collect
+#    modules - what sections of data to build
+#    index - database of all electronic filers provided by the IRS
+
+
+buildCore <- function( eins, years, form.type="990", modules="all", index=NULL )
+{
+  
+  library( dplyr )
+  library( xml2 )
+  
+  if( is.null(index) ) { index <- buildIndex() }
+  
+  # subset index
+  
+  these <- index[ index$EIN %in% eins & index$FilingYear %in% years & index$FormType %in% form.type , "URL" ]
+  
+  core <- NULL
+  
+  for( i in 1:length(these) )
+  {
+     one.npo <- scrapeXML( these[i], form.type, modules )
+     
+     core <- bind_rows( core, one.npo )
+  }
+  
+  return( core )
+
+}
---
+++ Helper Functions/getRevExp.R
@@ -0,0 +1,13 @@
+# FUNCTION TO COLLECT REVENUE AND EXPENSES MODULE
+#
+# Argument:  doc = xml document
+#
+# Return Value:  data frame
+
+
+
+
+getRevExp <- function( doc )
+{
+
+}
